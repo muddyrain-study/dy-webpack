015674df614d86d47cef1674823d06af61edbfad
‘muddyrain’ <qiu3291002845@gmail.com>
2023-05-17 22:25:32 +0800

    feat: babel 预设

---
+++ src/babel/index.js
@@ -0,0 +1,19 @@
+var arr = [1, 2, 3, 4, 5]
+
+for (const item of arr) {
+  console.log(item)
+}
+const obj = {
+  a: 1,
+  b: 3,
+}
+
+const { a, b } = obj
+
+class Person {
+  constructor() {
+    console.log('Person')
+  }
+}
+
+async function Test() {}
