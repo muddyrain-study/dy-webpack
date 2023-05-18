7dcaaa001212011d4f20fa717308db94d18fca48
‘muddyrain’ <qiu3291002845@gmail.com>
2023-05-18 21:51:38 +0800

    feat: babel 插件

--- src/babel/index.js
+++ src/babel/index.js
@@ -1,19 +1,3 @@
-var arr = [1, 2, 3, 4, 5]
-
-for (const item of arr) {
-  console.log(item)
+class A {
+  method() {}
 }
-const obj = {
-  a: 1,
-  b: 3,
-}
-
-const { a, b } = obj
-
-class Person {
-  constructor() {
-    console.log('Person')
-  }
-}
-
-async function Test() {}
